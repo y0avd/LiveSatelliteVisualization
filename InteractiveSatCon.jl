56f3017f639a5ce40e2cd53cb127bc7f5967d64f
Yoav Dekel <yoavdekel@airev.aero>
2023-04-14 10:19:06 -0400

    Selection Range w/ text box

--- InteractiveSatCon.jl
+++ InteractiveSatCon.jl
@@ -1,15 +1,15 @@
-# Initializations
-using GLMakie, FileIO, GeometryBasics, Colors, LinearAlgebra, DifferentialEquations
+# INITIALIZATION
+using GLMakie, FileIO, GeometryBasics, Colors, LinearAlgebra, DifferentialEquations, VideoIO
 
 include("OrbitFunctions.jl")
 include("PlottingFunctions.jl")
 
-# constants
+# CONSTANTS
 const μ = 398600.4418 # [km^3/s^2] Earth's graviation parameter
 const R⨁ = 6378.137 # [km] Earth's equatorial radius
 const ω⨁ = 2pi/(24*3600); # [rad/sec] Earth angular rotation
 
-# config
+# CONFIG
 screen_res = (1920, 1080)
 sats_opacity = 1
 orbits_opacity = 0.25
@@ -18,6 +18,10 @@ max_sats = 500 # cannot be below 1
 max_sma_R⨁ = 10 # cannot be below 1
 max_inc = 90 # cannot be below 0
 max_e = 0.9 # cannot be below 0
+
+buttonwidth = 120
+
+# FUNCTIONS
 
 function getNp(Ns, percent)
     # for a given Ns, Np can only be factor of Ns
@@ -55,28 +59,7 @@ function getNc(Np, percent)
     return Nc_options[idx]
 end
 
-# Observables from interactive (sets default)
-slider_vals_obs = Observable((1, 50., 50.,
-    1.5 < max_sma_R⨁ ? 1.5 : max_sma_R⨁,
-    max_inc/2, 0.))
-
-# Observables from satellite
-Ns = Observable(slider_vals_obs[][1])
-Np = Observable(getNp(Ns[],slider_vals_obs[][2]))
-Nc = Observable(getNc(Np[],slider_vals_obs[][3]))
-a = Observable(R⨁*slider_vals_obs[][4])
-i = Observable(deg2rad(slider_vals_obs[][5]))
-e = Observable(slider_vals_obs[][6])
-
-# Observables from plotting
-azimuth = Observable(0.4)
-elevation = Observable(0.75)
-
-# Observables for plotting
-title_strA = Observable("")
-title_strB = Observable("")
-
-function IsSelected(nump)
+function resetSelection(nump)
     boolarr = []
 
     for _ in 1:nump
@@ -100,20 +83,6 @@ function SatColor(bool_array, opacity, nump)
 
     return col
 end
-
-isSelected = Observable(IsSelected(Ns[]))
-
-satOpacity = Observable(sats_opacity)
-orbitOpacity = Observable(orbits_opacity)
-
-sats2d_color = @lift SatColor($isSelected, 1, $Ns)
-sats3d_color = @lift SatColor($isSelected, $satOpacity, $Ns)
-orbits_color = @lift ((:blue, $orbitOpacity))
-
-## PLOTTING ##
-
-f = Figure(resolution = screen_res)
-display(f)
 
 function updateFig(slider_vals)
     # save camera angles
@@ -155,23 +124,52 @@ function Sliders(slider_vals)
     end
 end
 
+function SelectSatsRange()
+    temp = []
+
+    if selection_end[] == 0
+        if selection_start[] == 0
+            return
+        else
+            for idx in 1:Ns[]
+                push!(temp, idx == selection_start[] ? true : false)
+            end
+        end
+    else
+        if selection_start[] == 0
+            return
+        else
+            for idx in 1:Ns[]
+                push!(temp, idx >= selection_start[] && idx <= selection_end[] ? true : false)
+            end
+        end
+    end
+
+    isSelected[] = temp
+end
+
 function Buttons()
     # Buttons
     f[2, 1] = menusgrid = GridLayout()
 
-    button = menusgrid[1:2, 1:2] = [
-        Button(f, label="Update\n3D Plot", buttoncolor = RGBf(0.8, 0.94, 0.8)),
-        Button(f, label="Random\nOrbit", buttoncolor = RGBf(0.94, 0.84, 0.84)),
-        Button(f, label="Orbits\nToggle", buttoncolor = RGBf(0.94, 0.94, 0.94)),
-        Button(f, label="Satellites\nToggle", buttoncolor = RGBf(0.94, 0.94, 0.94))]
+    button = menusgrid[1:3, 1:3] = [
+        Button(f, label="Render\nAnimation", width = buttonwidth, buttoncolor = RGBf(0.94, 0.94, 0.94)),
+        Button(f, label="Random\nConstellation", width = buttonwidth, buttoncolor = RGBf(0.94, 0.84, 0.84)),
+        Button(f, label="Update\n3D Figure", width = buttonwidth, buttoncolor = RGBf(0.8, 0.94, 0.8)),
+        Button(f, label="Place\nHolder", width = buttonwidth, buttoncolor = RGBf(0.94, 0.94, 0.94)),
+        Button(f, label="Show\nSatellites", width = buttonwidth, buttoncolor = RGBf(0.94, 0.94, 0.94)),
+        Button(f, label="Show\nOrbits", width = buttonwidth, buttoncolor = RGBf(0.94, 0.94, 0.94)),
+        Button(f, label="Reset\nSelection", width = buttonwidth, buttoncolor = RGBf(0.94, 0.94, 0.94)),
+        Textbox(f, placeholder = "$(selection_start[])", width = buttonwidth, reset_on_defocus = true, validator = Int64),
+        Textbox(f, placeholder = "$(selection_end[])", width = buttonwidth, reset_on_defocus = true, validator = Int64)]
     
-    # Update 3D Plot
-    on(button[1].clicks) do click
-        updateFig(slider_vals_obs[])
+    # Render Animation
+    on(button[1].clicks) do _
+    
     end
 
     # Random Orbit
-    on(button[2].clicks) do click
+    on(button[2].clicks) do _
         svals = [max_sats - 1, 100., 100., max_sma_R⨁ - 1, max_inc, max_e]
 
         for idx in 1:6
@@ -197,8 +195,27 @@ function Buttons()
         updateFig(slider_vals_obs[])
     end
 
+    # Update 3D Plot
+    on(button[3].clicks) do _
+        updateFig(slider_vals_obs[])
+    end
+
+    # Placeholder
+    on(button[4].clicks) do _
+        
+    end
+
+    # Satellites Toggle
+    on(button[5].clicks) do _
+        if satOpacity[] == sats_opacity
+            satOpacity[] = 0.0
+        else
+            satOpacity[] = sats_opacity
+        end
+    end
+
     # Orbits Toggle
-    on(button[3].clicks) do click
+    on(button[6].clicks) do _
         if orbitOpacity[] == orbits_opacity
             orbitOpacity[] = 0.0
         else
@@ -206,13 +223,39 @@ function Buttons()
         end
     end
 
-    # Satellites Toggle
-    on(button[4].clicks) do click
-        if satOpacity[] == sats_opacity
-            satOpacity[] = 0.0
-        else
-            satOpacity[] = sats_opacity
+    # Reset Selection
+    on(button[7].clicks) do _
+        isSelected[] = resetSelection(Ns[])
+        button[8].stored_string = "0"
+        button[9].stored_string = "0"
+    end
+
+    # Start Selection Text Box
+    on(button[8].stored_string) do s
+        int = parse(Int64,s)
+        if int < 0
+            button[8].stored_string = "0"
+        elseif int > max_sats
+            button[8].stored_string = "$max_sats"
+        elseif int > selection_end[] && selection_end[] != 0
+            button[8].stored_string = "$(selection_end[])"
+        else 
+            selection_start[] = int
         end
+        SelectSatsRange()
+    end
+
+    # End Selection Text Box
+    on(button[9].stored_string) do s
+        int = parse(Int64,s)
+        if int < selection_start[]
+            button[9].stored_string = "$(selection_start[])"
+        elseif int > max_sats
+            button[9].stored_string = "$max_sats"
+        else 
+            selection_end[] = int
+        end
+        SelectSatsRange()
     end
 end
 
@@ -248,7 +291,6 @@ function setUp(slider_vals)
     # creating observables for plotting
     cartPos = Observable([Vector{Point{3, Float32}}()])
 
-    # Layout Content
     # 2D
     gB = f[1, 3] = GridLayout()
     axB = Axis(gB[1,1], title = title_strB,
@@ -317,5 +359,45 @@ function setUp(slider_vals)
         end
     end
 end
+
+## OBSERVABLES
+
+# Observables from interactive (sets default)
+slider_vals_obs = Observable((1, 50., 50.,
+    1.5 < max_sma_R⨁ ? 1.5 : max_sma_R⨁,
+    max_inc/2, 0.))
+
+# Observables from satellite
+Ns = Observable(slider_vals_obs[][1])
+Np = Observable(getNp(Ns[],slider_vals_obs[][2]))
+Nc = Observable(getNc(Np[],slider_vals_obs[][3]))
+a = Observable(R⨁*slider_vals_obs[][4])
+i = Observable(deg2rad(slider_vals_obs[][5]))
+e = Observable(slider_vals_obs[][6])
+
+# Observables from plotting
+azimuth = Observable(0.4)
+elevation = Observable(0.75)
+
+# Observables for plotting
+title_strA = Observable("")
+title_strB = Observable("")
+
+isSelected = Observable(resetSelection(Ns[]))
+
+selection_start = Observable(0)
+selection_end = Observable(0)
+
+satOpacity = Observable(sats_opacity)
+orbitOpacity = Observable(orbits_opacity)
+
+sats2d_color = @lift SatColor($isSelected, 1, $Ns)
+sats3d_color = @lift SatColor($isSelected, $satOpacity, $Ns)
+orbits_color = @lift ((:blue, $orbitOpacity))
+
+## PLOTTING ##
+
+f = Figure(resolution = screen_res)
+display(f)
 
 setUp(slider_vals_obs[]);
